34f0a7e670b545cf6650a4c475b3dfb14281756d
Felipe Borges <felipetecacc@gmail.com>
2024-05-11 15:24:30 -0300

    create:model_user

--- public/index.php
+++ public/index.php
@@ -1,5 +1,11 @@
 <?php
 
-require_once(dirname(__FILE__, 2)) . '/src/config/database.php';
+require_once(dirname(__FILE__, 2)) . '/src/config/config.php';
+require_once(dirname(__FILE__, 2)) . '/src/models/User.php';
 
-Database::getConnection();
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
+print_r($user);
+echo '<br><br>';
+$user->email = 'lucas_alterado@cod3r.com.br';
+print_r($user->email);
+
