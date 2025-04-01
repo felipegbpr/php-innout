a0e30fb5f99903361715f20d02b48722880fe1c1
Felipe Borges <felipetecacc@gmail.com>
2025-04-01 17:54:35 -0300

    created: login_controller and loaders

--- public/index.php
+++ public/index.php
@@ -1,19 +1,6 @@
 <?php
 
 require_once(dirname(__FILE__, 2)) . '/src/config/config.php';
-// require_once(VIEW_PATH . '/login.php');
+require_once(CONTROLLER_PATH . '/login.php');
 
-/* TEST ***/
-require_once(MODEL_PATH . '/Login.php');
 
-$login = new Login([
-  'email' => 'admin@cod3r.com.br',
-  'password' => 'a'
-]);
-
-try {
-  $login->checkLogin();
-  echo 'Login Success!'; 
-} catch (Exception $e) {
-  echo 'Login Failed!'; 
-}
