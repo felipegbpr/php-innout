ddc47fd5469aad21d89178a4547a8c51673e7c9b
Felipe Borges <felipetecacc@gmail.com>
2025-04-02 15:53:21 -0300

    created: header and footer template files

---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <span>Desenvolvido com</span>
+  <span><i class="icofont-heart text-info mx-1"></i></span>
+  <span>por Comp<span class="text-info">4</span>ny Name</span>
+</footer>
+</body>
+</html>
