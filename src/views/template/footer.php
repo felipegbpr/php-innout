b31017c27ba044c6b787a4983237edea89f982a1
Felipe Borges <felipetecacc@gmail.com>
2025-04-03 14:59:30 -0300

    created: function (js) to hide sidebar menu

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -3,5 +3,6 @@
   <span><i class="icofont-heart text-info mx-1"></i></span>
   <span>por Comp<span class="text-info">4</span>ny Name</span>
 </footer>
+<script src="assets/js/app.js"></script>
 </body>
 </html>
