1c150ede97efe278049e45c1e3058b7d562cd539
Felipe Borges <felipetecacc@gmail.com>
2025-04-14 12:43:26 -0300

    implementation: displaying management report data

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,3 +1,9 @@
 <main class="content">
-  <h1>Ok</h1>
+  <?php
+    echo "Qtde de Usuários Ativos: {$activeUsersCount}";
+    echo "<br>";
+    echo "Horas Trabalhadas no mês: {$hoursInMonth}";
+    echo "<br>";
+    print_r($absentUsers);
+  ?>
 </main>
